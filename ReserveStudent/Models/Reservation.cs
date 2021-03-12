a172c512fa9f4d11a225ad60995f92c821e7ea9e
othmane-qr <othmane.qorqach@gmail.com>
2021-03-12 09:27:40 +0100

    Ajoutez des fichiers projet.

---
+++ ReserveStudent/Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReserveStudent.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("RequestingStudentId")]
+        public Student RequestingStudent { get; set; }
+        public string RequestingStudentId { get; set; }
+        [ForeignKey("ReservationTypeId")]
+        public ReservationType ReservationType { get; set; }
+        public int ReservationTypeId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
